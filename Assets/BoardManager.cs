d281d57829df1de4299f7e5d03c13015ba8a4832
DNguyen143 <dang.nguyen2210737cs@hcmut.edu.vn>
2024-11-18 19:08:38 +0700

    boardManager

---
+++ Assets/BoardManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    [SerializeField] public Dictionary<Vector2Int, Slot> slotDictionary = new Dictionary<Vector2Int, Slot>();
+
+    
+    void Update()
+    {
+        foreach (Slot slot in FindObjectsOfType<Slot>())
+        {
+            Vector2Int coords = new Vector2Int(slot.x, slot.y);
+            if (!slotDictionary.ContainsKey(coords)) slotDictionary.Add(coords, slot);
+            
+        }
+    }
+}

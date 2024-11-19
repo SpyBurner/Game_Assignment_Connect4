4e7f8494fc9690aaf07331fb4bda3bd581a64f99
Nguyễn Thành Nhân <142470690+Fkiril@users.noreply.github.com>
2024-11-19 09:42:45 +0700

    Update: background music

--- Assets/Scripts/Game/SoundManager.cs
+++ Assets/Scripts/Game/SoundManager.cs
@@ -8,20 +8,17 @@ public class SoundManager : MonoBehaviour
 
     void Start()
     {
-        // Tìm hoặc thêm Audio Source vào GameObject
         audioSource = GetComponent<AudioSource>();
         if (audioSource == null)
         {
             audioSource = gameObject.AddComponent<AudioSource>();
         }
 
-        // Liên kết Audio Clip với Audio Source
         audioSource.clip = ClickSound;
     }
 
     public void PlayClickSound()
     {
-        // Phát âm thanh
         audioSource.Play();
     }
 }

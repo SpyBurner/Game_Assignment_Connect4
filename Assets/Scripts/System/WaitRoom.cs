54668f6c8f27dffb22c666f73ea61aba2870b0f2
SpyBurner <zombiepigman1505@gmail.com>
2024-11-19 09:51:11 +0700

    last

--- Assets/Scripts/System/WaitRoom.cs
+++ Assets/Scripts/System/WaitRoom.cs
@@ -10,7 +10,7 @@ public class WaitRoom : MonoBehaviourPunCallbacks
     public Text winnerText;
 
     public int minPlayers = 2;
-    public int maxPlayers = 4;
+    public int maxPlayers = 2;
 
     // Start is called before the first frame update
     void Start()
